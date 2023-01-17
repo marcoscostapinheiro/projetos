4086355bc74221fc48b5543e9504644942f349aa
Marcos Costa Pinheiro <115197895+Marcos-Costa-Pinheiro@users.noreply.github.com>
2023-01-17 16:23:22 -0300

    jogo da velha pronto

--- projeto-tic_tac_toe/script.js
+++ projeto-tic_tac_toe/script.js
@@ -1,29 +1,31 @@
 var vez = 'X'
+var iguais = 0
 var sort = Math.floor(Math.random() * 2 + 1)
 var jogadas = [
     '', '', '',
     '', '', '',
     '', '', ''
 ]
-var comb = [
-    '0 1 2',
-    '3 4 5',
-    '6 7 8',
-    '0 3 6',
-    '1 4 7',
-    '2 5 8',
-    '0 4 8',
-    '2 4 6'
-]
+var cont = 0
+var comb = 0
 
 if(sort == 0){
     vez = 'X'
 }
-if(sort == 1){
+
+else{
     vez = 'O'
 }
 
-document.getElementById('player').innerText += ` ${vez}`
+if(vez == 'X'){
+    document.getElementById('player').style.color = '#E00D2A'
+}
+
+else{
+    document.getElementById('player').style.color = '#4860E0'
+}
+
+document.getElementById('player').innerText = vez
 
 function insert(c){
     var campo = document.getElementById(c).innerText
@@ -35,22 +37,94 @@ function insert(c){
     }
     if(campo == false){
         jogadas[c] += vez
+        cont ++
         document.getElementById(c).innerText += vez
-        if(vez == 'X'){
-            vez = 'O'
-            document.getElementById('player').innerText = `Vez de ${vez}`
-        }
-        else{
-            vez = 'X'
-            document.getElementById('player').innerText = `Vez de ${vez}`
-        }
+        change()
     }
+
     victory()
+    draw()
+    
 }
 
 function victory(){
-    
+    if(jogadas[0] == 'X' && jogadas[1] == 'X' && jogadas[2] == 'X' || jogadas[3] == 'X' && jogadas[4] == 'X' && jogadas[5] == 'X' || jogadas[6] == 'X' && jogadas[7] == 'X' && jogadas[8] == 'X' || jogadas[0] == 'X' && jogadas[3] == 'X' && jogadas[6] == 'X' || jogadas[1] == 'X' && jogadas[4] == 'X' && jogadas[7] == 'X' || jogadas[2] == 'X' && jogadas[5] == 'X' && jogadas[8] == 'X' || jogadas[0] == 'X' && jogadas[4] == 'X' && jogadas[8] == 'X' || jogadas[2] == 'X' && jogadas[4] == 'X' && jogadas[6] == 'X'){
+
+        comb = 1
+
+        setTimeout(function(){
+            window.alert('Jogador X venceu')
+        }, 100)
+
+        setTimeout(function(){
+            for(var pos in jogadas){
+                document.getElementById(pos).innerText = ''
+            }
+        }, 100)
+
+        jogadas = [
+            '', '', '',
+            '', '', '',
+            '', '', ''
+        ]
+    }
+
+    if(jogadas[0] == 'O' && jogadas[1] == 'O' && jogadas[2] == 'O' || jogadas[3] == 'O' && jogadas[4] == 'O' && jogadas[5] == 'O' || jogadas[6] == 'O' && jogadas[7] == 'O' && jogadas[8] == 'O' || jogadas[0] == 'O' && jogadas[3] == 'O' && jogadas[6] == 'O' || jogadas[1] == 'O' && jogadas[4] == 'O' && jogadas[7] == 'O' || jogadas[2] == 'O' && jogadas[5] == 'O' && jogadas[8] == 'O' || jogadas[0] == 'O' && jogadas[4] == 'O' && jogadas[8] == 'O' || jogadas[2] == 'O' && jogadas[4] == 'O' && jogadas[6] == 'O'){
+
+        comb = 1
+
+        setTimeout(function(){
+            window.alert('Jogador O venceu')
+        }, 100)
+
+        setTimeout(function(){
+            for(var pos in jogadas){
+                document.getElementById(pos).innerText = ''
+            }
+        }, 100)
+
+        jogadas = [
+            '', '', '',
+            '', '', '',
+            '', '', ''
+        ]
+    }
 }
+
+function change(){
+    if(vez == 'X'){
+        vez = 'O'
+        document.getElementById('player').innerText = vez
+        document.getElementById('player').style.color = '#4860E0'
+        
+    }
+    else{
+        vez = 'X'
+        document.getElementById('player').innerText = vez
+        document.getElementById('player').style.color = '#E00D2A'
+    }
+}
+
+function draw(){
+    if(cont == 9 && comb == 0){
+        setTimeout(function(){
+            window.alert('EMPATE')
+        }, 100)
+
+        setTimeout(function(){
+            for(var pos in jogadas){
+                document.getElementById(pos).innerText = ''
+            }
+        }, 100)
+
+        jogadas = [
+            '', '', '',
+            '', '', '',
+            '', '', ''
+        ]
+    }
+}
+
 
 
 
