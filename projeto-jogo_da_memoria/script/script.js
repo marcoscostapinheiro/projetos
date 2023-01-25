f6613807c15d597ee38d3addbc56fb6db492517a
Marcos Costa Pinheiro <115197895+Marcos-Costa-Pinheiro@users.noreply.github.com>
2023-01-25 14:07:03 -0300

    tamanho redefinido no mobile e funçao duplicate e shuffledArray foram atualizadas

---
+++ projeto-jogo_da_memoria/script/script.js
@@ -0,0 +1,50 @@
+var interface = document.getElementById('interface')
+
+var characters = ['morty',
+    'rick',
+    'summer',
+    'jerry',
+    'beth',
+    'jessica',
+    'pessoa-passaro',
+    'rick-picles',
+]
+
+var createElement = (tag, className) => {
+    var element = document.createElement(tag)
+    element.className = className
+    return element
+}
+
+var createCard = () => {
+    for(item in characters){
+        var card = createElement('div', 'card')
+        var front = createElement('div', 'face front')
+        var back = createElement('div', 'face back')
+
+        card.appendChild(back)
+        card.appendChild(front)
+        interface.appendChild(card)
+
+        front.style.background =  `white url(imagens/${characters[item]}.jpg) center`
+        front.style.backgroundSize = 'cover'
+    }
+}
+
+var duplicateArray = () => {
+    for(item in characters){
+        characters.push(characters[item])
+    }
+}
+
+var shuffledArray = () => {
+    characters.sort(()=> Math.random() - 0.5)
+}
+
+var loadGame = () => {
+    duplicateArray()
+    shuffledArray()
+    createCard()
+}
+
+loadGame()
