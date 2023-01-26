20f99da5e52302e26fc65ccabce21d93fe3d6ef4
Marcos Costa Pinheiro <115197895+Marcos-Costa-Pinheiro@users.noreply.github.com>
2023-01-25 23:05:26 -0300

    jogo da memoria finalizado

--- projeto-jogo_da_memoria/script/script.js
+++ projeto-jogo_da_memoria/script/script.js
@@ -10,6 +10,63 @@ var characters = ['morty',
     'rick-picles',
 ]
 
+var firstCard = ''
+var secondCard = ''
+var combination = 0
+
+var checkGame = () => {
+    if(combination === 8){
+        combination = 0
+        setTimeout(function(){
+            window.alert('VOCÊ ENCONTROU TODAS AS COMBINAÇÕES')
+        }, 400)
+        setTimeout(function(){
+            for(item in interface.children){
+                interface.children[item].classList.remove('card-reveal')
+            }
+        }, 400)
+    }
+    
+}
+
+var checkCombination = () => {
+    var firstCharacter = firstCard.getAttribute('data-character')
+    var secondCharacter = secondCard.getAttribute('data-character')
+
+    if(firstCharacter === secondCharacter){
+        firstCard.classList.add('found')
+        secondCard.classList.add('found')
+        firstCard = ''
+        secondCard = ''
+        combination ++
+        checkGame()
+    }
+    else{
+        setTimeout(function(){
+            firstCard.classList.remove('card-reveal')
+            secondCard.classList.remove('card-reveal')
+            firstCard = ''
+            secondCard = ''
+        }, 500)
+    }
+}
+
+var revealCard = ( {target} ) => {
+    if(target.parentNode.className.includes('card-reveal')){
+        return
+    }
+    
+    if(firstCard === ''){
+        target.parentNode.classList.add('card-reveal')
+        firstCard = target.parentNode
+    }
+    else if(secondCard === ''){
+        target.parentNode.classList.add('card-reveal')
+        secondCard = target.parentNode
+        checkCombination()
+    }
+}
+
 var createElement = (tag, className) => {
     var element = document.createElement(tag)
     element.className = className
@@ -25,9 +82,12 @@ var createCard = () => {
         card.appendChild(back)
         card.appendChild(front)
         interface.appendChild(card)
+        card.setAttribute('data-character', characters[item])
 
         front.style.background =  `white url(imagens/${characters[item]}.jpg) center`
         front.style.backgroundSize = 'cover'
+
+        card.addEventListener('click', revealCard)
     }
 }
 
@@ -48,3 +108,4 @@ var loadGame = () => {
 }
 
 loadGame()
+
